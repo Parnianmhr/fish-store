e8968145d99a6c4a8d52d6b228abd98d4ac8306d
Parnian <parnian@parnians-mbp.home>
2017-05-11 14:55:45 +0200

    component Order and inventory made

---
+++ src/components/Inventory.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react'
+
+class Inventory extends PureComponent {
+
+  render() {
+    return(
+      <p>Inventory</p>
+    )
+  }
+}
+
+export default Inventory
---
+++ src/components/Order.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react'
+
+class Order extends PureComponent {
+
+  render() {
+    return(
+      <p>Order</p>
+    )
+  }
+}
+
+export default Order
