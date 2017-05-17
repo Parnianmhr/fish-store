56e8c16bae93184caabc06348630498fe78bab97
Parnian <parnian@parnians-mbp.home>
2017-05-17 23:11:28 +0200

    getting the name of the store in first page

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,12 +2,21 @@ import React, { PureComponent } from 'react'
 import { getFunName } from '../helpers'
 
 class StorePicker extends PureComponent {
+  goToStore(event) {
+    event.preventDefault();
+    //grab the text from the box
+    console.log(this.storeInput.value)
+    //transition form to store page
+
+  }
+
   render() {
     return(
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
         <h2>Please enter a store</h2>
-        <input type="text" required placeholder="Store name" defaultValue={getFunName()}/>
-        <button type="submit">Visit Store -></button>
+        <input type="text" required placeholder="Store name"
+          defaultValue={getFunName()} ref={(input) => { this.storeInput = input }}/>
+        <button type="submit">Visit Store --></button>
       </form>
     )
   }
